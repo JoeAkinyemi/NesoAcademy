9d7e86f2704b0480ed5fc8343540b9f7b63c57d5
Joe <104761730+JoeAkinyemi@users.noreply.github.com>
2022-08-14 21:04:42 +0100

    added calloc function

---
+++ NESO ACADEMY/calloc.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n, i, *p;
+    printf("Enter the total number of values: ");
+    scanf("%d", &n);
+
+    p = (int*)calloc(n,sizeof(int));
+    
+    printf("\nenter the values:");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", (p+i));
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("%d\t", *(p+i));
+    }
+    free(p);
+}
