8ea1eb93d14a0c66e47989a9152bfb208eb73cb3
Joe <104761730+JoeAkinyemi@users.noreply.github.com>
2022-08-15 05:11:37 +0100

    added the realloc file

---
+++ NESO ACADEMY/realloc.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n, i, j, *p;
+    printf("Enter the total number of values: ");
+    scanf("%d", &n);
+    /* Memory allocation */
+    p = (int*)malloc(n*sizeof(int));
+    
+    printf("\nEnter the values:");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", (p+i));
+    }
+    /* Reallocation of the memory*/
+    printf("\nEnter the updated size of n: ");
+    scanf("%d", &j);
+
+    int *p1= (int*)realloc(p,j*sizeof(int));
+    /* this the loop that orignal memory that was allocated*/
+    for(i=0; i<n; i++)
+    {
+        printf("%d\t", *(p+i));
+    }
+    /* this the loop that will prinnt the new size of allocated memory*/
+    for(i=0; i<j; i++)
+    {
+        printf("\n%d\t", *(p1+i));
+    }
+    free(p1);
+}
