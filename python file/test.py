d98268719d4ecc8dfa09220d44398d4a71aa29e7
Joe <104761730+JoeAkinyemi@users.noreply.github.com>
2022-09-26 20:58:47 +0100

    test file

---
+++ python file/test.py
@@ -0,0 +1,10 @@
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+
+a = set('abracadabra')
+b = set('alacazam')
+
+print(a)
+
+a = {x for x in 'abracadabra' if x not in 'abc'}
+print(a)
